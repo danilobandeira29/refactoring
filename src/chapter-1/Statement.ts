a4645cc5ec2a73fceacdc96ee891cb484c828471
danilo <danilobandeira29@gmail.com>
2021-10-09 14:34:28 -0300

    refactor: change function declaration

--- src/chapter-1/Statement.ts
+++ src/chapter-1/Statement.ts
@@ -12,16 +12,16 @@ export default function statement (invoice: Invoice, plays: Play) {
         // add volume credits
         volumeCredits += volumeCreditsFor(perf)
         // print line for this order
-        result += ` ${playsFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+        result += ` ${playsFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
-    function format(value: number): string {
+    function usd(value: number): string {
         return new Intl.NumberFormat("en-US",
-            { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(value)
+            { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(value/100)
     }
 
     function playsFor(p: Performance) {
