f099497330216861e6f973d51db233b745043d22
danilo <danilobandeira29@gmail.com>
2021-10-09 12:39:44 -0300

    feat: add Statement

---
+++ src/chapter-1/Statement.ts
@@ -0,0 +1,44 @@
+import Invoice from "./interfaces/Invoice";
+import Play from "./interfaces/Plays";
+import { PlayType } from "./interfaces/PlayType";
+
+export default function statement (invoice: Invoice, plays: Play) {
+    let totalAmount = 0
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat("en-US",
+        { style: "currency", currency: "USD",
+            minimumFractionDigits: 2 }).format;
+
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        let thisAmount = 0;
+        switch (play.type) {
+            case PlayType.TRAGEDY:
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case PlayType.COMEDY:
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+        // add volume credits
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if (PlayType.COMEDY === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        // print line for this order
+        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+    return result;
+}
