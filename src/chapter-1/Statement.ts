837589187a92813c9e61d6299c48dc8b95042e0b
danilo <danilobandeira29@gmail.com>
2021-10-10 15:39:41 -0300

    feat: change statement to html

--- src/chapter-1/Statement.ts
+++ src/chapter-1/Statement.ts
@@ -3,24 +3,25 @@ import Play from "./interfaces/Plays";
 import Statement from "./interfaces/Statement";
 import createStatement from "./createStatement";
 
-export default function statement (invoice: Invoice, plays: Play) {
-    return renderPlainText(createStatement(invoice, plays));
+export default function htmlStatement (invoice: Invoice, plays: Play) {
+    return renderHtml(createStatement(invoice, plays));
 }
 
-function renderPlainText(statement: Statement) {
-    let result = `Statement for ${statement.customer}\n`;
-
+function renderHtml(statement: Statement) {
+    let result = `<h1>Statement for ${statement.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>\n";
     for (let perf of statement.performances) {
-        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>\n`;
+        result += ` <td>${usd(perf.amount)}</td>\n`;
     }
-
-    result += `Amount owed is ${usd(statement.totalAmount)}\n`;
-    result += `You earned ${statement.totalVolumeCredits} credits\n`;
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(statement.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${statement.totalVolumeCredits}</em> credits</p>\n`;
     return result;
+}
 
-    function usd(value: number): string {
-        return new Intl.NumberFormat("en-US",
-            { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(value/100)
-    }
-
+function usd(value: number): string {
+    return new Intl.NumberFormat("en-US",
+        { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(value/100)
 }
