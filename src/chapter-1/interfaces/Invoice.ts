b2b291e662fe671e0bebc0dd74d3808ceb7b4fc2
danilo <danilobandeira29@gmail.com>
2021-10-10 15:27:12 -0300

    refactor: remove unnecessary types

--- src/chapter-1/interfaces/Invoice.ts
+++ src/chapter-1/interfaces/Invoice.ts
@@ -3,6 +3,4 @@ import Performance from "./Performance";
 export default interface Invoice {
     customer: string;
     performances: Performance[];
-    totalAmount: number;
-    totalVolumeCredits: number;
 }
