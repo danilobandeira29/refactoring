368562c9122608c4d0cf4c49efe117d2c250a671
danilo <danilobandeira29@gmail.com>
2021-10-11 20:00:22 -0300

    refactor: move function

--- src/chapter-1/CreateStatement.ts
+++ src/chapter-1/CreateStatement.ts
@@ -12,6 +12,28 @@ class PerformanceCalculator {
         this.performance = performance;
         this.play = play;
     }
+
+    get amount(): number {
+        let amount = 0;
+        switch (this.play.type) {
+            case PlayType.TRAGEDY:
+                amount = 40000;
+                if (this.performance.audience > 30) {
+                    amount += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case PlayType.COMEDY:
+                amount = 30000;
+                if (this.performance.audience > 20) {
+                    amount += 10000 + 500 * (this.performance.audience - 20);
+                }
+                amount += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${this.performance.play.type}`);
+        }
+        return amount;
+    }
 }
 
 export default function createStatement(invoice: Invoice, plays: Play) {
@@ -26,35 +48,13 @@ export default function createStatement(invoice: Invoice, plays: Play) {
         const calculator = new PerformanceCalculator(aPerformance, playsFor(aPerformance));
         const performance: Performance = { ...aPerformance };
         performance.play = calculator.play;
-        performance.amount = amountFor(performance);
+        performance.amount = calculator.amount;
         performance.volumeCredits = volumeCreditsFor(performance);
         return performance;
     }
 
     function playsFor(p: Performance) {
         return plays[p.playID];
-    }
-
-    function amountFor(performance: Performance): number {
-        let amount = 0;
-        switch (performance.play.type) {
-            case PlayType.TRAGEDY:
-                amount = 40000;
-                if (performance.audience > 30) {
-                    amount += 1000 * (performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                amount = 30000;
-                if (performance.audience > 20) {
-                    amount += 10000 + 500 * (performance.audience - 20);
-                }
-                amount += 300 * performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${performance.play.type}`);
-        }
-        return amount;
     }
 
     function volumeCreditsFor(performance: Performance) {
