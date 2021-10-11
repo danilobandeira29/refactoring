78f94e3e57ac832bd231a74e0c4b9afedc940e01
danilo <danilobandeira29@gmail.com>
2021-10-11 20:06:50 -0300

    refactor: move function

--- src/chapter-1/CreateStatement.ts
+++ src/chapter-1/CreateStatement.ts
@@ -2,46 +2,7 @@ import Invoice from "./interfaces/Invoice";
 import Play from "./interfaces/Plays";
 import Statement from "./interfaces/Statement";
 import Performance from "./interfaces/Performance";
-import { PlayType } from "./interfaces/PlayType";
-
-class PerformanceCalculator {
-    performance: Performance;
-    play: { name: string; type: PlayType };
-
-    constructor(performance: Performance, play: { name: string, type: PlayType }) {
-        this.performance = performance;
-        this.play = play;
-    }
-
-    get amount(): number {
-        let amount = 0;
-        switch (this.play.type) {
-            case PlayType.TRAGEDY:
-                amount = 40000;
-                if (this.performance.audience > 30) {
-                    amount += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case PlayType.COMEDY:
-                amount = 30000;
-                if (this.performance.audience > 20) {
-                    amount += 10000 + 500 * (this.performance.audience - 20);
-                }
-                amount += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.performance.play.type}`);
-        }
-        return amount;
-    }
-
-    get volumeCreditsFor() {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(this.performance.audience - 30, 0);
-        if (PlayType.COMEDY === this.play.type) volumeCredits += Math.floor(this.performance.audience / 5);
-        return volumeCredits;
-    }
-}
+import PerformanceCalculator from "./PerformanceCalculator";
 
 export default function createStatement(invoice: Invoice, plays: Play) {
     const statement: Statement = {} as Statement;
