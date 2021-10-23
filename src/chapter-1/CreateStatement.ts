3fde3a0c3948f6eae7ee10e974848af2a8201f58
danilo <danilobandeira29@gmail.com>
2021-10-23 13:03:32 -0300

    refactor: move function

--- src/chapter-1/CreateStatement.ts
+++ src/chapter-1/CreateStatement.ts
@@ -9,37 +9,37 @@ import TragedyCalculator from "./TragedyCalculator";
 export default function createStatement(invoice: Invoice, plays: Play) {
     const statement: Statement = {} as Statement;
     statement.customer = invoice.customer;
-    statement.performances = invoice.performances.map(enrichPerformance);
+    statement.performances = invoice.performances.map((p) => enrichPerformance(p, plays));
     statement.totalAmount = totalAmount(statement);
     statement.totalVolumeCredits = totalVolumeCredits(statement);
     return statement;
+}
 
-    function enrichPerformance(aPerformance: Performance) {
-        const calculator = createPerformanceCalculator(aPerformance, playsFor(aPerformance));
-        const performance: Performance = { ...aPerformance };
-        performance.play = calculator.play;
-        performance.amount = calculator.amount();
-        performance.volumeCredits = calculator.volumeCreditsFor();
-        return performance;
-    }
+function enrichPerformance(aPerformance: Performance, plays: Play) {
+    const calculator = createPerformanceCalculator(aPerformance, playsFor(aPerformance, plays));
+    const performance: Performance = { ...aPerformance };
+    performance.play = calculator.play;
+    performance.amount = calculator.amount();
+    performance.volumeCredits = calculator.volumeCreditsFor();
+    return performance;
+}
 
-    function createPerformanceCalculator(p: Performance, play: { name: string, type: PlayType }) {
-        switch (play.type){
-            case PlayType.COMEDY: return new ComedyCalculator(p, play);
-            case PlayType.TRAGEDY: return new TragedyCalculator(p, play);
-            default: throw new Error(`unknown type ${play.type}`);
-        }
+function createPerformanceCalculator(p: Performance, play: { name: string, type: PlayType }) {
+    switch (play.type){
+        case PlayType.COMEDY: return new ComedyCalculator(p, play);
+        case PlayType.TRAGEDY: return new TragedyCalculator(p, play);
+        default: throw new Error(`unknown type ${play.type}`);
     }
+}
 
-    function playsFor(p: Performance) {
-        return plays[p.playID];
-    }
+function playsFor(p: Performance, plays: Play) {
+    return plays[p.playID];
+}
 
-    function totalAmount(statement: Statement) {
-        return statement.performances.reduce((acc, p) => acc + p.amount, 0);
-    }
+function totalAmount(statement: Statement) {
+    return statement.performances.reduce((acc, p) => acc + p.amount, 0);
+}
 
-    function totalVolumeCredits(statement: Statement) {
-        return statement.performances.reduce((acc, p) => acc + p.volumeCredits, 0);
-    }
+function totalVolumeCredits(statement: Statement) {
+    return statement.performances.reduce((acc, p) => acc + p.volumeCredits, 0);
 }
