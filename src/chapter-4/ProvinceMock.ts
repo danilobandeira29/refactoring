a96d9cd88695a7df03e4c7cdd1270281f214e3e6
danilo <danilobandeira29@gmail.com>
2021-10-24 17:14:25 -0300

    refactor: change function to class and add create static method

---
+++ src/chapter-4/ProvinceMock.ts
@@ -0,0 +1,16 @@
+import { ProvinceType } from "./types/Province";
+
+export default class ProvinceMock {
+    static create(): ProvinceType {
+        return {
+            name: "Asia",
+            producers: [
+                {name: "Byzantium", cost: 10, production: 9},
+                {name: "Attalia",   cost: 12, production: 10},
+                {name: "Sinope",    cost: 10, production: 6},
+            ],
+            demand: 30,
+            price: 20
+        };
+    }
+}
