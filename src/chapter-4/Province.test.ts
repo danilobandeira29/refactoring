495db0dd915952d7d21e5e88065b5281181eea01
danilo <danilobandeira29@gmail.com>
2021-10-25 20:47:11 -0300

    refactor: create new fixture Province before each

--- src/chapter-4/Province.test.ts
+++ src/chapter-4/Province.test.ts
@@ -3,8 +3,17 @@ import ProvinceMock from "./ProvinceMock";
 import * as assert from "assert";
 
 describe('Province suite', function() {
+    let asia: Province;
+
+    beforeEach(function () {
+        asia = new Province(ProvinceMock.create());
+    })
+
     it('shortfall', function() {
-        const asia = new Province(ProvinceMock.create());
         assert.equal(asia.getShortfall(), 5);
+    })
+
+    it('profit', function () {
+        assert.equal(asia.getProfit(), 230);
     })
 })
