37e55e57236a1f5fde637833f6958a389e41933e
danilo <danilobandeira29@gmail.com>
2021-10-26 20:00:35 -0300

    test: no producers shortfall

--- src/chapter-4/Province.test.ts
+++ src/chapter-4/Province.test.ts
@@ -1,24 +1,45 @@
 import Province from "./Province";
 import ProvinceMock from "./ProvinceMock";
+import {ProvinceType} from "./types/Province";
 
 describe("Province suite", function() {
-    let asia: Province;
+    describe("province", function() {
+        let asia: Province;
 
-    beforeEach(function() {
-        asia = new Province(ProvinceMock.create());
+        beforeEach(function() {
+            asia = new Province(ProvinceMock.create());
+        })
+
+        it("shortfall", function() {
+            expect(asia.getShortfall()).toEqual(5);
+        })
+
+        it("profit", function() {
+            expect(asia.getProfit()).toEqual(230);
+        })
+
+        it("change production", function() {
+            asia.producers[0].production = 20;
+            expect(asia.getShortfall()).toEqual(-6);
+            expect(asia.getProfit()).toEqual(292);
+        })
     })
 
-    it("shortfall", function() {
-        expect(asia.getShortfall()).toEqual(5);
-    })
+    describe("no producers", function() {
+        let noProducers: Province;
 
-    it("profit", function() {
-        expect(asia.getProfit()).toEqual(230);
-    })
+        beforeEach(function() {
+            const provinceWithoutProducers: ProvinceType = {
+                name: "no_producers",
+                producers: [],
+                demand: 30,
+                price: 20
+            }
+            noProducers = new Province(provinceWithoutProducers)
+        })
 
-    it("change production", function() {
-        asia.producers[0].production = 20;
-        expect(asia.getShortfall()).toEqual(-6);
-        expect(asia.getProfit()).toEqual(292);
+        it("shortfall", function() {
+            expect(noProducers.getShortfall()).toEqual(30)
+        })
     })
 })
