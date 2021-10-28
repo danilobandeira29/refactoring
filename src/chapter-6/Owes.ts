a38c5ac3e60489e18cffaf1c230b5a4055f875e9
danilo <danilobandeira29@gmail.com>
2021-10-27 22:02:51 -0300

    refactor: extract function calculateOutstanding

--- src/chapter-6/Owes.ts
+++ src/chapter-6/Owes.ts
@@ -2,14 +2,11 @@ import { Invoice } from "./interfaces";
 
 export default class Owes {
     printOwing(aInvoice: Invoice) {
-        let outstanding = 0;
 
         Owes.printBanner();
 
         // calcula o valor a pagar (outstanding)
-        for (const o of aInvoice.orders) {
-            outstanding += o.amount;
-        }
+        const outstanding = Owes.calculateOutstanding(aInvoice);
 
         // registra a data de vencimento (due date)
         const invoice = Owes.recordDueDate(aInvoice);
@@ -35,5 +32,13 @@ export default class Owes {
         const today = new Date("10/29/1993");
         invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
         return invoice;
+    }
+
+    private static calculateOutstanding(i: Invoice) {
+        let outstanding = 0;
+        for (const o of i.orders) {
+            outstanding += o.amount;
+        }
+        return outstanding;
     }
 }
