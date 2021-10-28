2871692d312c2f8c79704afe7be2cdd25c6f7a5b
danilo <danilobandeira29@gmail.com>
2021-10-27 22:00:48 -0300

    refactor: extract function recordDueDate

--- src/chapter-6/Owes.ts
+++ src/chapter-6/Owes.ts
@@ -1,19 +1,18 @@
 import { Invoice } from "./interfaces";
 
 export default class Owes {
-    printOwing(invoice: Invoice) {
+    printOwing(aInvoice: Invoice) {
         let outstanding = 0;
 
         Owes.printBanner();
 
         // calcula o valor a pagar (outstanding)
-        for (const o of invoice.orders) {
+        for (const o of aInvoice.orders) {
             outstanding += o.amount;
         }
 
         // registra a data de vencimento (due date)
-        const today = new Date("10/29/1993");
-        invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+        const invoice = Owes.recordDueDate(aInvoice);
 
         // exibe detalhes
         Owes.printDetails(invoice, outstanding);
@@ -29,5 +28,12 @@ export default class Owes {
         console.log(`name: ${invoice.customer}`);
         console.log(`amount: ${Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(outstanding)}`);
         console.log(`due: ${invoice.dueDate.toLocaleDateString("pt-BR")}`);
+    }
+
+    private static recordDueDate(i: Invoice) {
+        const invoice = { ...i };
+        const today = new Date("10/29/1993");
+        invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+        return invoice;
     }
 }
