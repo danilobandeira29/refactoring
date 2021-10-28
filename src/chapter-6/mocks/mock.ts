85e29f0fe77782c44c9bf8641867737b9de7261b
danilo <danilobandeira29@gmail.com>
2021-10-27 21:44:11 -0300

    feat: add invoice mock

---
+++ src/chapter-6/mocks/mock.ts
@@ -0,0 +1,14 @@
+import { Invoice } from "../interfaces";
+export default {
+    orders: [
+        {
+            amount: 4
+        },
+        {
+            amount: 5
+        }
+    ],
+    customer: "Danilo Bandeira",
+    dueDate: new Date("10/29/1993"),
+    amount: 4444,
+} as Invoice;
