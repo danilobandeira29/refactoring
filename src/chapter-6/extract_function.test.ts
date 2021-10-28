1c23de7bd378080a42ef7dd95fbe313f757dcf94
danilo <danilobandeira29@gmail.com>
2021-10-27 21:48:19 -0300

    test: should print details

--- src/chapter-6/extract_function.test.ts
+++ src/chapter-6/extract_function.test.ts
@@ -2,13 +2,21 @@ import Owes from "./extract_function";
 import invoiceMock from './mocks/mock';
 
 describe("extract function", function() {
-    it("should print", function() {
-        const owes = new Owes();
+    let owes: Owes;
+
+    beforeEach(function() {
+        owes = new Owes();
         console.log = jest.fn();
         owes.printOwing(invoiceMock);
+    })
+
+    it("should print banner", function() {
         expect(console.log).toHaveBeenNthCalledWith(1, "***********************");
         expect(console.log).toHaveBeenNthCalledWith(2, "**** Customer Owes ****");
         expect(console.log).toHaveBeenNthCalledWith(3, "***********************");
+    })
+
+    it("should print details", function() {
         expect(console.log).toHaveBeenNthCalledWith(4, "name: Danilo Bandeira");
         expect(console.log).toHaveBeenNthCalledWith(5, "amount: R$ 9,00");
         expect(console.log).toHaveBeenNthCalledWith(6, "due: 28/11/1993");
