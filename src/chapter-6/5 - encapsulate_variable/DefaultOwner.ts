594336bec0bc0d1ff86c8f87b693e9b1de7a1825
danilo <danilobandeira29@gmail.com>
2021-10-31 16:03:57 -0300

    feat: add export to defaultOwner and setDefaultOwner

--- src/chapter-6/5 - encapsulate_variable/DefaultOwner.ts
+++ src/chapter-6/5 - encapsulate_variable/DefaultOwner.ts
@@ -1,9 +1,9 @@
 let defaultOwnerData = { first_name: "Danilo", last_name: "Bandeira" };
 
-function defaultOwner() {
+export function defaultOwner() {
     return Object.assign({}, defaultOwnerData);
 }
 
-function setDefaultOwner(owner: { first_name: string; last_name: string; }) {
+export function setDefaultOwner(owner: { first_name: string; last_name: string; }) {
     defaultOwnerData = owner;
 }
