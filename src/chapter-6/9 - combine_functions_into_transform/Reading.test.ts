8dfd8e157e9377197261821aa6eeac94e507dbe1
danilo <danilobandeira29@gmail.com>
2021-11-04 21:33:03 -0300

    test: should reading original must be unchanged

--- src/chapter-6/9 - combine_functions_into_transform/Reading.test.ts
+++ src/chapter-6/9 - combine_functions_into_transform/Reading.test.ts
@@ -1,12 +1,15 @@
 import { enrichReading } from "./Reading";
 import ReadingMock from "./mocks/ReadingMock";
 import EnrichReading from "./interfaces/EnrichReading";
+import ReadingRecord from "./interfaces/ReadingRecord";
 
 describe("Reading suite", function() {
+    let readingOriginal: ReadingRecord;
     let reading: EnrichReading;
 
     beforeEach(function() {
-        reading = enrichReading(ReadingMock.create());
+        readingOriginal = ReadingMock.create();
+        reading = enrichReading(readingOriginal);
     })
 
     it("should calculate base charge", function() {
@@ -15,5 +18,9 @@ describe("Reading suite", function() {
 
     it("should calculate taxable charge", function() {
         expect(reading.taxable_charge).toEqual(0.19999999999999998);
+    })
+
+    it("should reading original must be unchanged", function() {
+        expect(readingOriginal).not.toEqual(reading);
     })
 })
