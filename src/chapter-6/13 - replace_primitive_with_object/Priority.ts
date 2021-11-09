1d1b4543443e5f0d205eadd03e1f234329901487
danilo <danilobandeira29@gmail.com>
2021-11-08 21:18:30 -0300

    feat: add Priority

---
+++ src/chapter-6/13 - replace_primitive_with_object/Priority.ts
@@ -0,0 +1,23 @@
+export default class Priority {
+    private readonly value: string;
+
+    constructor(value: string) {
+        this.value = value;
+    }
+
+    toString() {
+        return this.value;
+    }
+
+    static getIndex(value: string): number {
+        return Priority.legalValues().findIndex(p => p === value);
+    }
+
+    static legalValues() {
+        return ["low", "normal", "high", "rush"];
+    }
+
+    highThan(value: string) {
+        return Priority.getIndex(this.toString()) > Priority.getIndex(value);
+    }
+}
