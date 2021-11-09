1adc5c49be01e2883b51266b00a30676145c95b9
danilo <danilobandeira29@gmail.com>
2021-11-08 21:18:45 -0300

    test: High Order Count Suite

---
+++ src/chapter-6/13 - replace_primitive_with_object/HighOrderCount.test.ts
@@ -0,0 +1,13 @@
+import Order from "./Order";
+
+describe("High Order Count Suite", function() {
+    it("should return 2 its the count of high priority orders", function() {
+        const highOrdersCount: number = [
+            new Order("high"),
+            new Order("rush"),
+            new Order("normal"),
+            new Order("normal"),
+        ].filter(o => o.getPriority().highThan('normal')).length;
+        expect(highOrdersCount).toEqual(2);
+    })
+})
