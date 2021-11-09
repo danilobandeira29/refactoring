86cc8e82e539b0f73b784614ddde8bdff0a6f058
danilo <danilobandeira29@gmail.com>
2021-11-08 21:50:55 -0300

    test: should return 3 its the count of orders that priority is equal to normal and should return 1 its the count of orders that priority is lower than normal

--- src/chapter-6/13 - replace_primitive_with_object/HighOrderCount.test.ts
+++ src/chapter-6/13 - replace_primitive_with_object/HighOrderCount.test.ts
@@ -1,13 +1,37 @@
 import Order from "./Order";
 
 describe("High Order Count Suite", function() {
-    it("should return 2 its the count of high priority orders", function() {
-        const highOrdersCount: number = [
+    it("should return 2 its the count of orders high than normal", function() {
+        const highThanNormalOrdersCount: number = [
             new Order("high"),
             new Order("rush"),
             new Order("normal"),
             new Order("low"),
         ].filter(o => o.getPriority().highThan("normal")).length;
-        expect(highOrdersCount).toEqual(2);
+        expect(highThanNormalOrdersCount).toEqual(2);
+    })
+
+    it("should return 3 its the count of orders that priority is equal to normal", function() {
+        const ordersEqualsNormalCount: number = [
+            new Order("high"),
+            new Order("rush"),
+            new Order("normal"),
+            new Order("normal"),
+            new Order("normal"),
+            new Order("low"),
+        ].filter(o => o.getPriority().equals("normal")).length;
+        expect(ordersEqualsNormalCount).toEqual(3);
+    })
+
+    it("should return 1 its the count of orders that priority is lower than normal", function() {
+        const lowerThanNormalOrdersCount: number = [
+            new Order("high"),
+            new Order("rush"),
+            new Order("normal"),
+            new Order("normal"),
+            new Order("normal"),
+            new Order("low"),
+        ].filter(o => o.getPriority().lowerThan("normal")).length;
+        expect(lowerThanNormalOrdersCount).toEqual(1);
     })
 })
