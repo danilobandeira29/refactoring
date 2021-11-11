245afc3bc642475db6bdca9ecede7a2ffec3cf7c
danilo <danilobandeira29@gmail.com>
2021-11-11 20:29:10 -0300

    refactor: change method declaration

--- src/chapter-6/16 - inline_class/Shipment_OLD.ts
+++ src/chapter-6/16 - inline_class/Shipment_OLD.ts
@@ -1,13 +1,9 @@
 import TrackingInformation_OLD, { TrackingInformationDto } from "./TrackingInformation_OLD";
 
 export default class Shipment_OLD {
-    private tracking: TrackingInformation_OLD;
+    tracking: TrackingInformation_OLD;
 
     constructor(tracking_dto: TrackingInformationDto) {
         this.tracking = new TrackingInformation_OLD(tracking_dto);
-    }
-
-    getTracking() {
-        return this.tracking.display();
     }
 }
