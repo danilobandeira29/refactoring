b833a1ec92ffde46ac1a96b7dc0cf10f4c4987ee
danilo <danilobandeira29@gmail.com>
2021-11-11 20:41:08 -0300

    refactor: rename variable

--- src/chapter-6/16 - inline_class/Shipment_OLD/Shipment_OLD.test.ts
+++ src/chapter-6/16 - inline_class/Shipment_OLD/Shipment_OLD.test.ts
@@ -2,10 +2,10 @@ import Shipment_OLD from "./Shipment_OLD";
 
 describe("Shipment_OLD", function() {
     it("should return tracking information that's company name and tracking number", function() {
-        const shipment = {
+        const tracking_dto = {
             company_name: "DB",
             tracking_number: 4444
         };
-        expect(new Shipment_OLD(shipment).tracking.display()).toEqual("DB: 4444");
+        expect(new Shipment_OLD(tracking_dto).tracking.display()).toEqual("DB: 4444");
     })
 })
