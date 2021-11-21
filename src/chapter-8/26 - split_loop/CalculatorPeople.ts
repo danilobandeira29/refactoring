01c9f91b4eefb1334654a709fe462794354191d2
danilo <danilobandeira29@gmail.com>
2021-11-21 15:06:43 -0300

    refactor: slide statements

--- src/chapter-8/26 - split_loop/CalculatorPeople.ts
+++ src/chapter-8/26 - split_loop/CalculatorPeople.ts
@@ -1,11 +1,11 @@
 import PeopleDto from "./PeopleDto";
 
 export default function calculatorPeople(people: PeopleDto[]): string {
-    let youngest_age = people[0].age || Infinity;
     let total_salary = 0;
     for(const p of people) {
         total_salary += p.salary;
     }
+    let youngest_age = people[0].age || Infinity;
     for(const p of people) {
         if (p.age < youngest_age) youngest_age = p.age;
     }
