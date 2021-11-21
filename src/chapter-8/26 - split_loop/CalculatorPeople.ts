cf48078d02579e3d71a6bac0e4d25228b36ff760
danilo <danilobandeira29@gmail.com>
2021-11-21 15:09:49 -0300

    refactor: extract function and replace temp with query

--- src/chapter-8/26 - split_loop/CalculatorPeople.ts
+++ src/chapter-8/26 - split_loop/CalculatorPeople.ts
@@ -1,13 +1,18 @@
 import PeopleDto from "./PeopleDto";
 
 export default function calculatorPeople(people: PeopleDto[]): string {
-    let total_salary = 0;
-    for(const p of people) {
-        total_salary += p.salary;
-    }
+    const total_salary = totalSalary(people);
     let youngest_age = people[0].age || Infinity;
     for(const p of people) {
         if (p.age < youngest_age) youngest_age = p.age;
     }
     return `youngest_age: ${youngest_age}, total_salary: ${total_salary}`;
+}
+
+function totalSalary(people: PeopleDto[]): number {
+    let total_salary = 0;
+    for(const p of people) {
+        total_salary += p.salary;
+    }
+    return total_salary;
 }
