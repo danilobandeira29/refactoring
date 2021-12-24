70e26297553cb64b74ac7acb8d062e427f5581fa
danilo <danilobandeira29@gmail.com>
2021-12-24 18:17:12 -0300

    test: add Bird Factory Suite

---
+++ src/chapter-12/59-replace_subclasses_with_delegate/BirdFactory.test.ts
@@ -0,0 +1,22 @@
+import EuropeanSwallow from "./EuropeanSwallow";
+import createBird from "./BirdFactory";
+import AfricanSwallow from "./AfricanSwallow";
+import NorwegianBlueParrot from "./NorwegianBlueParrot";
+
+describe("Bird Factory Suite", function() {
+    test("should create EuropeanSwallow", function() {
+        expect(createBird({ name: "EuropeanSwallow", type: "EuropeanSwallow" })).toBeInstanceOf(EuropeanSwallow);
+    });
+
+    test("should create AfricanSwallow", function() {
+        expect(createBird({ name: "AfricanSwallow", type: "AfricanSwallow", numberOfCoconuts: 4 })).toBeInstanceOf(AfricanSwallow);
+    });
+
+    test("should create NorwegianBlueParrot", function() {
+        expect(createBird({ name: "NorwegianBlueParrot", type: "NorwegianBlueParrot"})).toBeInstanceOf(NorwegianBlueParrot);
+    });
+
+    test("should throw when try create a non-existent Bird type", function() {
+        expect(() => createBird({ name: "NonExistentType", type: "NonExistentType" as any })).toThrow("Non-existent Bird type.");
+    });
+})
