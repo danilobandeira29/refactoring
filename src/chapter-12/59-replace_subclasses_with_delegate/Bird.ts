6594622a0ea81fb0d4d5cb1699fe11e642b3e36f
danilo <danilobandeira29@gmail.com>
2021-12-24 18:37:29 -0300

    feat: add speciesDelegate and selectSpeciesDelegate

--- src/chapter-12/59-replace_subclasses_with_delegate/Bird.ts
+++ src/chapter-12/59-replace_subclasses_with_delegate/Bird.ts
@@ -1,15 +1,20 @@
+import EuropeanSwallowDelegate from "./EuropeanSwallowDelegate";
+
 export interface BirdDto {
     name: string;
     plumage?: string;
+    type?: string;
 }
 
 export default class Bird {
     private readonly _name: string;
     private readonly _plumage: string;
+    readonly speciesDelegate: EuropeanSwallowDelegate | null;
 
-    constructor({ name, plumage }: BirdDto) {
-        this._name = name;
-        this._plumage = plumage || "average";
+    constructor(dto: BirdDto) {
+        this._name = dto.name;
+        this._plumage = dto.plumage || "average";
+        this.speciesDelegate = Bird.selectSpeciesDelegate(dto);
     }
 
     get plumage(): string {
@@ -17,6 +22,14 @@ export default class Bird {
     }
 
     get airSpeedVelocity(): null | number {
-        return null;
+        return this.speciesDelegate ? this.speciesDelegate.airSpeedVelocity : null;
+    }
+
+    private static selectSpeciesDelegate(dto: BirdDto): EuropeanSwallowDelegate | null {
+        switch(dto.type) {
+            case "EuropeanSwallow":
+                return new EuropeanSwallowDelegate();
+            default: return null;
+        }
     }
 }
