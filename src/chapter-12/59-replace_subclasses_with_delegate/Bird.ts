bb22e3e6b3fa2a1783b2bc73eb8cd744b1947572
danilo <danilobandeira29@gmail.com>
2021-12-24 19:28:18 -0300

    feat: uses NorwegianBlueParrot and SpeciesDelegate

--- src/chapter-12/59-replace_subclasses_with_delegate/Bird.ts
+++ src/chapter-12/59-replace_subclasses_with_delegate/Bird.ts
@@ -1,39 +1,48 @@
 import EuropeanSwallowDelegate from "./EuropeanSwallowDelegate";
 import AfricanSwallowDelegate, {AfricanSwallowDelegateDto} from "./AfricanSwallowDelegate";
+import NorwegianBlueParrotDelegate, {NorwegianBlueParrotDelegateDto} from "./NorwegianBlueParrotDelegate";
+import SpeciesDelegateImpl from "./SpeciesDelegate";
 
 export interface BirdDto {
     name: string;
     plumage?: string;
     type?: string;
     numberOfCoconuts?: number;
+    voltage?: number;
+    isNailed?: boolean;
 }
 
+type SpeciesDelegate = EuropeanSwallowDelegate | AfricanSwallowDelegate | NorwegianBlueParrotDelegate | SpeciesDelegateImpl ;
+
 export default class Bird {
-    private readonly _name: string;
-    private readonly _plumage: string;
-    readonly speciesDelegate: EuropeanSwallowDelegate | AfricanSwallowDelegate | null;
+    readonly name: string;
+    readonly _plumage: string;
+    readonly speciesDelegate: SpeciesDelegate;
 
     constructor(dto: BirdDto) {
-        this._name = dto.name;
+        this.name = dto.name;
         this._plumage = dto.plumage || "average";
-        this.speciesDelegate = Bird.selectSpeciesDelegate(dto);
+        this.speciesDelegate = this.selectSpeciesDelegate(dto);
     }
 
     get plumage(): string {
-        return this._plumage;
+        return this.speciesDelegate.plumage;
     }
 
-    get airSpeedVelocity(): null | number {
-        return this.speciesDelegate ? this.speciesDelegate.airSpeedVelocity : null;
+    get airSpeedVelocity(): number | null {
+        return this.speciesDelegate.airSpeedVelocity;
     }
 
-    private static selectSpeciesDelegate(dto: BirdDto): EuropeanSwallowDelegate | null {
+    private selectSpeciesDelegate(dto: BirdDto): SpeciesDelegate {
         switch(dto.type) {
             case "EuropeanSwallow":
-                return new EuropeanSwallowDelegate();
+                return new EuropeanSwallowDelegate(this);
             case "AfricanSwallow":
-                return new AfricanSwallowDelegate(dto as AfricanSwallowDelegateDto);
-            default: return null;
+                return new AfricanSwallowDelegate(dto as AfricanSwallowDelegateDto, this);
+            case "NorwegianBlueParrot":
+                return new NorwegianBlueParrotDelegate(dto as NorwegianBlueParrotDelegateDto, this);
+            default:
+                return new SpeciesDelegateImpl({ props: dto, bird: this });
         }
     }
 }
