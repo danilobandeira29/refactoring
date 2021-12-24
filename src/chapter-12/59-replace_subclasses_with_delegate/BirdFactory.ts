9cd13438436b310e4be87fcdc83227f7e0ffc2a4
danilo <danilobandeira29@gmail.com>
2021-12-24 18:16:58 -0300

    feat: add Bird Factory Suite

---
+++ src/chapter-12/59-replace_subclasses_with_delegate/BirdFactory.ts
@@ -0,0 +1,22 @@
+import Bird, {BirdDto} from "./Bird";
+import EuropeanSwallow from "./EuropeanSwallow";
+import AfricanSwallow, {AfricanSwallowDto} from "./AfricanSwallow";
+import NorwegianBlueParrot, {NorwegianBlueParrotDto} from "./NorwegianBlueParrot";
+
+export type Birds = BirdDto | AfricanSwallowDto | NorwegianBlueParrotDto;
+
+type CreateBirdDto = Birds & {
+    type: "EuropeanSwallow" | "AfricanSwallow" | "NorwegianBlueParrot";
+}
+
+export default function createBird(dto: CreateBirdDto): Bird {
+    switch (dto.type) {
+        case "EuropeanSwallow" :
+            return new EuropeanSwallow(dto);
+        case "AfricanSwallow":
+            return new AfricanSwallow(dto as AfricanSwallowDto);
+        case "NorwegianBlueParrot":
+            return new NorwegianBlueParrot(dto as NorwegianBlueParrotDto);
+        default: throw new Error("Non-existent Bird type.");
+    }
+}
