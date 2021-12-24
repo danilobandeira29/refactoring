3eb9403cb1fd851b5910c5b146c899b7daebd740
danilo <danilobandeira29@gmail.com>
2021-12-24 17:38:16 -0300

    feat: add NorwegianBlueParrot

---
+++ src/chapter-12/59-replace_subclasses_with_delegate/NorwegianBlueParrot.ts
@@ -0,0 +1,25 @@
+import Bird, {BirdDto} from "./Bird";
+
+interface NorwegianBlueParrotDto extends BirdDto {
+    voltage: number;
+    isNailed: boolean;
+}
+
+export default class NorwegianBlueParrot extends Bird {
+    private readonly _voltage: number;
+    private readonly _isNailed: boolean;
+
+    constructor(dto: NorwegianBlueParrotDto) {
+        super(dto);
+        this._voltage = dto.voltage;
+        this._isNailed = dto.isNailed;
+    }
+
+    get airSpeedVelocity(): number | null {
+        return this._isNailed ? 0 : 10 + this._voltage / 10;
+    }
+
+    get plumage(): string {
+        return this._voltage > 100 ? "scorched" : "beautiful";
+    }
+}
